7895fe64db71c0be7151d46578bf14cad959e18b
Tom <106112216+cabbagetom@users.noreply.github.com>
2023-03-20 18:45:06 +0100

    first version of first redux app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import {useSelector, useDispatch} from "react-redux";
-import { decrement, increment } from "./counter";
+import { decrement, increment, incrementByAmount } from "./counter";
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
   const count = useSelector((state)=>state.counter.value);
+  const [userInput, setUserInput] = useState(0);
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(incrementByAmount(Number(userInput)));
+    setUserInput(0)
+  };
+
+
+
   const dispatch = useDispatch();
 
   return (
     <div>
+
     <div className="App">
       <h1>{count}</h1>
     </div>
@@ -15,6 +28,19 @@ function App() {
       <button onClick={()=>dispatch(increment())}>Increment</button>
       <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
+
+    <form className ="Form" onClick={handleSubmit}>
+    <label>
+    Value:
+    <input 
+      type = "text"
+      name = "value"
+      onChange={(e)=> setUserInput(e.target.value)}
+      value={userInput}
+    />
+    </label>
+    <button type="submit">Submit</button>
+    </form>
     </div>
   );
 }
